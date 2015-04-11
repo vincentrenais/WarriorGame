01279fb276b9e28dce224c3e7c8cd5c718a4ebdf
Vincent Renais <vincentrenais@gmail.com>
2015-04-11 15:47:26 -0400

    Initial Commit

---
+++ WarriorGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WarriorGame
+//
+//  Created by Vincent Renais on 2015-04-11.
+//  Copyright (c) 2015 Vincent Renais. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
